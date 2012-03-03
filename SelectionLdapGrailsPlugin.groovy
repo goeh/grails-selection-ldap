fe8b81a7ce0e011bda022d3cab20e383db6e60c5
Goran Ehrsson <goran@technipelago.se>
2012-03-03 19:42:55 +0100

    Changed dependency to selection 0.5.2

--- SelectionLdapGrailsPlugin.groovy
+++ SelectionLdapGrailsPlugin.groovy
@@ -16,11 +16,11 @@
  */
 class SelectionLdapGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [selection:'0.4 > *']
+    def dependsOn = [selection:'0.5 > *']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
